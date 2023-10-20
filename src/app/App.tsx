c60e47d6d8dffb072079c4242eac3312e093cd65
Nikita Lomeiko <lomieiko78@mail.ru>
2023-10-21 00:26:22 +0300

    feat: add tailwind & router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,17 @@
 import "./styles/index.scss";
+import {IconButton} from "../shared/ui/icon-button";
+import TestIcon from "shared/assets/icons/test.svg?react";
 
 export const App = () => {
   return (
     <>
-      <div>Good template</div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <h1>
+            Hello world!
+        </h1>
+        <IconButton Icon={TestIcon}/>
     </>
   )
 }
