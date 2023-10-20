f9473beef92eaf2beed4c21a7477fd44f7c8592e
Nikita Lomeiko <lomieiko78@mail.ru>
2023-10-20 20:49:44 +0300

    feat - add fsd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import './index.css'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { App } from 'app/App.tsx';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
