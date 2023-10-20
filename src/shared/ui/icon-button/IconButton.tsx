2673e8e98e69052475b7599d404e1073f7a7e76b
Nikita Lomeiko <lomieiko78@mail.ru>
2023-10-21 02:19:04 +0300

    feat: add ui btns

---
+++ src/shared/ui/icon-button/IconButton.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import style from "./IconButton.module.scss";
+import classNames from "classnames";
+
+interface IIconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+    Icon: React.VFC<React.SVGProps<SVGSVGElement>>
+    className?: string,
+    width?: number,
+    bgColor?: string,
+}
+
+export const IconButton = (props: IIconButtonProps) => {
+    const {
+        Icon,
+        className,
+        width = 30,
+        bgColor = "#c4c4c4",
+        ...otherProps
+    } = props;
+
+    const [isHover, setIsHover] = useState(false);
+
+    return (
+        <button
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            style={{backgroundColor: !isHover ? bgColor : `${bgColor}80`}}
+            className={classNames(style.btn, className)} {...otherProps}>
+
+            <Icon width={`${width}px`} className={className}/>
+        </button>
+    );
+};
---
+++ src/shared/ui/text-button/TextButton.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import style from "./TextButton.module.scss";
+import classNames from "classnames";
+
+interface ITextButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string,
+    children: React.ReactNode,
+    styled?: 'filled' | 'outline',
+    bgColor?: string,
+    color?: string,
+    hoverColor?: string,
+    width?: string,
+    padding?: string
+}
+
+export const TextButton = (props: ITextButtonProps) => {
+    const {
+        children,
+        styled = 'filled',
+        bgColor = "#e4e4e4",
+        color = "#ffffff",
+        hoverColor = "#ffffff",
+        className,
+        width = "150px",
+        padding = "5px",
+        ...otherProps
+    } = props;
+
+    const [isHover, setIsHover] = useState(false);
+
+    return (
+        <div style={{width: width}}>
+            <button
+                onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}
+                style={styled === "filled" ? !isHover ? {backgroundColor: bgColor, color: color, padding: padding} : {backgroundColor: `${bgColor}80`, color: hoverColor, padding: padding} : !isHover ? { border: `1px solid ${bgColor}`, color: color, padding: padding} : {backgroundColor: `${bgColor}80`, color: hoverColor, padding: padding}} className={classNames(style.btn, className)} {...otherProps}>
+                {children}
+            </button>
+        </div>
+    );
+};
