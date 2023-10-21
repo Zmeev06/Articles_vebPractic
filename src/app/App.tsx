73450da902cf2d7f64428e0e4f3fb65acb6a55ac
Zmeev06 <se.rg.ey06@icloud.com>
2023-10-21 12:15:06 +0300

    feat: add ...

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
+import ArticleWidgets from 'widgets/article/ArticleWidgets'
 import './styles/index.scss'
 import { Input } from 'shared/ui/input/Input'
-
+import { Autocomplete } from 'shared/ui/autocomplete/Autocomplete'
 
 export const App = () => {
+  const suggestions = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
   return (
     <>
+      {/* <ArticleWidgets /> */}
+      <Autocomplete suggestions={suggestions} />
     </>
   )
 }
