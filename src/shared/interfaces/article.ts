da6de1daf27d5272960e983a227877d752aac6f3
NikitaTorbenko <2015nekitciti@gmail.com>
2023-10-22 01:57:53 +0300

    feat

--- src/shared/interfaces/article.ts
+++ src/shared/interfaces/article.ts
@@ -1,9 +1,11 @@
 export interface IArticle {
   content: string
+  cover: string
   created_at: string
   id: number
   normalised_title: string
   subtitle: string
   theme: string
   title: string
+  times_visited: number
 }
--- src/widgets/article/ui/ArticleWidgets.tsx
+++ src/widgets/article/ui/ArticleWidgets.tsx
@@ -1,54 +1,54 @@
-import style from "./ArticleWidgets.module.scss";
-
-import ShareIcon from "shared/assets/icons/share.svg?react";
-import DateIcon from "shared/assets/icons/date.svg?react";
-import EyeIcon from "shared/assets/icons/eye.svg?react";
-import { IconButton } from 'shared/ui/icon-button';
-import { useEffect, useRef, useState } from "react";
+import DateIcon from 'shared/assets/icons/date.svg?react'
+import EyeIcon from 'shared/assets/icons/eye.svg?react'
+import ShareIcon from 'shared/assets/icons/share.svg?react'
+import { IconButton } from 'shared/ui/icon-button'
+import style from './ArticleWidgets.module.scss'
 
 interface IArticleWidgetsProps {
-  theme?: string,
-  color?: string,
-  title: string,
-  subtitle: string,
-  content: string,
-  date: string,
-  viewCount: number,
+  theme?: string
+  color?: string
+  title: string
+  subtitle: string
+  content: string
+  date: string
+  viewCount: number
 }
 
 export const ArticleWidgets = (props: IArticleWidgetsProps) => {
   const {
-    theme = "#e4e4e4",
-    color = "#333333",
+    theme = '#e4e4e4',
+    color = '#333333',
     subtitle,
     title,
     content,
     date,
-    viewCount
-  } = props;
+    viewCount,
+  } = props
 
   return (
-    <div style={{backgroundColor: theme}} className={style.article}>
+    <div style={{ backgroundColor: theme }} className={style.article}>
       <div className={style.title_block}>
         <h1 className={style.title}>{title}</h1>
-        <IconButton width={25} height={25} Icon={ShareIcon}/>
+        <IconButton width={15} height={15} Icon={ShareIcon} />
       </div>
       <div className={style.article_info}>
         <div className={style.date}>
-          <DateIcon/>
+          <DateIcon />
           <div>{date}</div>
         </div>
         <div className={style.views}>
-          <EyeIcon/>
+          <EyeIcon />
           <div>{viewCount}</div>
         </div>
       </div>
       <h2 className={style.subtitle}>{subtitle}</h2>
 
-      <div className={style.bar} style={{backgroundColor: color}}></div>
+      <div className={style.bar} style={{ backgroundColor: color }} />
 
-      <div className={style.content} dangerouslySetInnerHTML={{__html: content}}>  
-      </div>
+      <div
+        className={style.content}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
   )
 }
