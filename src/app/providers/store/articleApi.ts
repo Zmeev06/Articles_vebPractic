cd24e617cfc1152b03421ae25af1798263cf3c37
NikitaTorbenko <2015nekitciti@gmail.com>
2023-10-21 17:12:45 +0300

    feat: add all endpoints

--- src/app/providers/store/articleApi.ts
+++ src/app/providers/store/articleApi.ts
@@ -1,11 +1,37 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { IArticle } from 'shared/interfaces/article'
+
+interface CreateArticle {
+  content: 'string'
+  subtitle: 'string'
+  theme: 'string'
+  title: 'string'
+}
 
 export const articleApi = createApi({
   reducerPath: 'articleApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://195.2.93.178/api' }),
   endpoints: (builder) => ({
-    getArticle: builder.query<null, void>({
-      query: () => ``,
+    getArticles: builder.query<IArticle[], void>({
+      query: () => `/articles`,
+    }),
+    getArticleByTitle: builder.query<IArticle, string>({
+      query: (id) => `article/${id}`,
+    }),
+    getArticleQrCode: builder.query<IArticle, number>({
+      query: (id) => `qr-codes/${id}`,
+    }),
+    createArticle: builder.mutation<IArticle, CreateArticle>({
+      query: ({ content, subtitle, theme, title }) => ({
+        url: `create/article`,
+        method: 'POST',
+        body: {
+          content,
+          subtitle,
+          theme,
+          title,
+        },
+      }),
     }),
   }),
 })
---
+++ src/shared/interfaces/article.ts
@@ -0,0 +1,9 @@
+export interface IArticle {
+  content: string
+  created_at: string
+  id: number
+  normalised_title: string
+  subtitle: string
+  theme: string
+  title: string
+}
