afd7a3c64fda3954ae15f6bb2ec7a1f3457337fa
Zmeev06 <se.rg.ey06@icloud.com>
2023-10-21 13:13:50 +0300

    feat: create article

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,14 @@ import ArticleWidgets from 'widgets/article/ArticleWidgets'
 import './styles/index.scss'
 import { Input } from 'shared/ui/input/Input'
 import { Autocomplete } from 'shared/ui/autocomplete/Autocomplete'
+import { CreateArticlePage } from 'pages/CreateArticlePage/CreateArticlePage'
 
 export const App = () => {
   const suggestions = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
   return (
     <>
       {/* <ArticleWidgets /> */}
-      <Autocomplete suggestions={suggestions} />
+      <CreateArticlePage />
     </>
   )
 }
