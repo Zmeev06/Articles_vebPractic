b7fff2fd14924c6d270fcebd619574fd7d6406b1
Zmeev06 <se.rg.ey06@icloud.com>
2023-10-21 03:07:05 +0300

    fix: prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,28 @@
-import "./styles/index.scss";
-import {IconButton} from "../shared/ui/icon-button";
-import TestIcon from "shared/assets/icons/test.svg?react";
-import {TextButton} from "../shared/ui/text-button";
+import './styles/index.scss'
+import TestIcon from 'shared/assets/icons/test.svg?react'
+import { IconButton } from '../shared/ui/icon-button'
+import { TextButton } from '../shared/ui/text-button'
 
 export const App = () => {
   return (
     <>
-
-        <IconButton bgColor="#000000" onClick={() => console.log("ывпывпывп")} Icon={TestIcon}/>
-        <TextButton color="#ffffff" bgColor="#000000" styled={"filled"}>-10см хую серёжи</TextButton>
-        <TextButton width="500px" padding="20px" color="#333333" bgColor="#000000" styled={"outline"}>-20см хую серёжи</TextButton>
+      <IconButton
+        bgColor="#000000"
+        onClick={() => console.log('ывпывпывп')}
+        Icon={TestIcon}
+      />
+      <TextButton color="#ffffff" bgColor="#000000" styled="filled">
+        -10см хую серёжи
+      </TextButton>
+      <TextButton
+        width="500px"
+        padding="20px"
+        color="#333333"
+        bgColor="#000000"
+        styled="outline"
+      >
+        -20см хую серёжи
+      </TextButton>
     </>
   )
 }
